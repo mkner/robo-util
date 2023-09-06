625c76d31c0906af11abf18bc378baef7991d206
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-09-06 17:50:08 -0400

    Update __init__.py

--- roboutils/__init__.py
+++ roboutils/__init__.py
@@ -3,11 +3,7 @@
 robo-utils  - useful functions for mobile robotics
 """
 
-# flit uses this as one source version #
-# note: pep whatever doesnt like > 'x.y.<number>c'
-# for example: 0.01.08d doesnt fly building from pyproject.toml
-
-__version__ = '0.02.03b' #prev dev '0.01.03c4'
+__version__ = '0.02.04' #prev 0.2.03b <- ...<- dev '0.01.03c4'
 
 # import acts to export 
 
