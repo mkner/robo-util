0cf146d1848f5038433660e8b20dbc2e18f0557a
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-11-03 17:04:06 -0400

    Update __init__.py

--- roboutils/__init__.py
+++ roboutils/__init__.py
@@ -3,7 +3,7 @@
 robo-utils  - useful functions for mobile robotics
 """
 
-__version__ = '0.02.06' #prev 0.2.03b <- ...<- dev '0.01.03c4'
+__version__ = '0.02.07a' #prev 0.2.03b <- ...<- dev '0.01.03c4'
 
 # import acts to export 
 
